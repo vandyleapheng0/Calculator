a8071948f1046380d4ea6398c7d2825c1831c8e7
dfg0 <80799956+vandyleapheng0@users.noreply.github.com>
2021-10-14 19:30:01 +0700

    Add files via upload
    Changing the style and the way widgets are place in the app.

--- main.py
+++ main.py
@@ -2,117 +2,181 @@ import tkinter as tk
 from tkinter import ttk, messagebox as msg
 
 
-class Calculator(tk.Tk):
+style_settings = {
+    "background": "black",
+    "foreground": "grey",
+    "button_bg": "#222222",
+    "button_fg": "white",
+    "button_active_bg": "#333333",
+    "button_active_fg": "white",
+    "button_press_bg": "#555555",
+    "button_press_fg": "white",
+    "font": ("JetBrains Mono", 17),
+    "select_bg": "#555555"
+}
 
+
+theme_settings = {
+    "TButton": {
+        "configure": {
+            "background": style_settings["button_bg"],
+            "foreground": style_settings["button_fg"],
+            "font": style_settings["font"],
+            "anchor": "center",
+            "focuscolor": "clear"
+        },
+        "map": {
+            "background": [
+                ("pressed", style_settings["button_press_bg"]),
+                ("active", style_settings["button_active_bg"])
+            ],
+            "foreground": [
+                ("pressed", style_settings["button_press_fg"]),
+                ("active", style_settings["button_active_fg"])
+            ]
+        }
+    },
+    "TEntry": {
+        "configure": {
+            "fieldbackground": style_settings["button_bg"],
+            "foreground": style_settings["button_fg"],
+            "selectbackground": style_settings["select_bg"],
+            "insertcolor": style_settings["button_fg"],
+            "borderwidth": 0
+        }
+    }
+}
+
+
+# layout
+# =============
+# (   )   %  /
+# 7   8   9  x
+# 4   5   6  -
+# 1   2   3  +
+# +/- 0   .  =
+
+class Calculator(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.geometry("400x400")
         self.title("Calculator")
-        self.config(bg="#0a0a0a")
-        self.attributes("-topmost", 3)
-        self.minsize(width=400, height=400)
-        self.iconbitmap("icons\\cal.ico")
+        self.geometry("400x400")
+        self.configure(bg=style_settings["button_bg"])
+        self.attributes("-topmost", 1)
+        self.minsize(200, 200)
 
-        # styling the button
+        self.make_gui()
+
+        # varibles
+        self.trantable = str.maketrans({"^": "**", "x": "*"})
+        self.valid_inputs = set("1234567890-+/xX*^.%()")
+        self.entry = self.nametowidget("entry")
+
         self.style = ttk.Style(self)
+        self.style.theme_create("dark.vista", settings=theme_settings)
+        self.style.theme_use("dark.vista")
 
-        black = "#1f1f1f"
-        font = ("JetBrains Mono", 20)
-        btn_clicked_bg = "#525252"
-        btn_clicked_fg = "grey"
-        btn_hover_bg = "#333333"
+        # binding shortcuts
+        self.bind("<BackSpace>", self.backspace)
+        self.bind("<Return>", lambda e: self.calc())
+        self.entry.bind("<BackSpace>", self.backspace)
+        self.entry.bind("<Return>", lambda e: self.calc())
+        self.entry.bind("<FocusOut>", lambda e: self.entry.icursor("end"))
 
-        style_settings = {
-            "TButton": {
-                "configure": {
-                    "background": black,
-                    "foreground": "white",
-                    "font": font,
-                    "focuscolor": "clear",
-                    "anchor": "center"
-                },
-                "map": {
-                    "background": [('pressed', btn_clicked_bg), ("active", btn_hover_bg)],
-                    "foreground": [('pressed', btn_clicked_fg), ("active", "white")]
-                }
-            }
-        }
-        self.style.theme_create("custom", settings=style_settings)
-        self.style.theme_use("custom")
+    def make_gui(self):
 
-        self.entry = tk.Entry(
-            self, validate="key", font=font, justify="right",
-            bg=black, fg="white", bd=0, insertbackground="white",
-            validatecommand=(self.register(self.validate), "%P")
-        )
-        self.entry.grid(row=0, column=0, columnspan=4, sticky="ewsn", ipady=10, padx=2, pady=(3, 2))
-
-        buttons = [
-            ['^', 'n²', 'c', '<--'],
-            ['(', ')', '%', '÷'],
-            ['7', '8', '9', 'x'],
-            ['4', '5', '6', '-'],
-            ['1', '2', '3', '+'],
-            ['+/-', '0', '.', '=']
+        # self.button_grid_opts = {}
+        # self.button_opts = {}
+        entry_validate_cmd = self.register(self.validate)
+        self.layout = [
+            [
+                [
+                    ttk.Entry(
+                        self, name="entry",
+                        justify="right",
+                        font=style_settings["font"],
+                        validate="key",
+                        validatecommand=(entry_validate_cmd, "%S")
+                    ),
+                    {"columnspan": 4, "padx": 4}
+                ]
+            ],
+            [
+                [ttk.Button(self, text="^")],
+                [ttk.Button(self, text="n²", command=lambda: self.add("^2"))],
+                [ttk.Button(self, text="c", command=lambda: self.entry.delete(0, "end"))],
+                [ttk.Button(self, text="<--", command=self.backspace)]
+            ],
+            [
+                [ttk.Button(self, text="(")],
+                [ttk.Button(self, text=")")],
+                [ttk.Button(self, text="%")],
+                [ttk.Button(self, text="/")]
+            ],
+            [
+                [ttk.Button(self, text="7")],
+                [ttk.Button(self, text="8")],
+                [ttk.Button(self, text="9")],
+                [ttk.Button(self, text="x")]
+            ],
+            [
+                [ttk.Button(self, text="4")],
+                [ttk.Button(self, text="5")],
+                [ttk.Button(self, text="6")],
+                [ttk.Button(self, text="-")]
+            ],
+            [
+                [ttk.Button(self, text="1")],
+                [ttk.Button(self, text="2")],
+                [ttk.Button(self, text="3")],
+                [ttk.Button(self, text="+")]
+            ],
+            [
+                [ttk.Button(self, text="+/-", command=lambda: self.calc(neget=True))],
+                [ttk.Button(self, text="0")],
+                [ttk.Button(self, text="·", command=lambda: self.add("."))],
+                [ttk.Button(self, text="=", command=self.calc)]
+            ]
         ]
 
-        for x in range(len(buttons[0])):
-            self.columnconfigure(x, weight=1)
-        for y in range(1, len(buttons) + 1):
-            self.rowconfigure(y, weight=1)
+        for row, widgets_info in enumerate(self.layout):
+            for col, widget_info in enumerate(widgets_info):
+                widget = widget_info[0]
+                grid_opts = {"row": row, "column": col, "sticky": "nswe"}
+                if len(widget_info) == 2:
+                    grid_opts.update(widget_info[1])
 
-        for row, buttons_ in enumerate(buttons, 1):
-            for col, btn_text in enumerate(buttons_):
-                command = self.get_func(btn_text)
-                button_ = ttk.Button(self, text=btn_text, command=command)
-                button_.grid(row=row, column=col, sticky="wesn", padx=2, pady=2)
+                if isinstance(widget, ttk.Button) and not widget.cget("command"):
+                    widget.config(
+                        command=lambda text=widget.cget("text"): self.add(text)
+                    )
+                widget.grid(grid_opts)
 
-        # Shortcuts
-        self.bind("<BackSpace>", self.backspace)
-        self.bind("<Return>", self.calculate)
+        for i in range(self.grid_size()[0]):
+            self.columnconfigure(i, weight=1)
+        for j in range(self.grid_size()[1]):
+            self.rowconfigure(j, weight=1)
 
-    def validate(self, value):
-        return all(x in "0123456789.-+x/*^%" for x in value)
-
-    def get_func(self, btn_text):
-        if btn_text == "<--":
-            return lambda: self.backspace()
-        elif btn_text == "c":
-            return lambda: self.clear()
-        elif btn_text == "n²":
-            return lambda: self.add_to_entry("^2")
-        elif btn_text == "+/-":
-            return self.negat
-        elif btn_text == "=":
-            return lambda: self.calculate()
-        else:
-            return lambda s=btn_text: self.add_to_entry(s)
-
-    def negat(self):
-        self.calculate()
-        if "-" not in self.entry.get():
-            self.entry.insert(0, "-")
-        else:
-            self.entry.delete(0)
-
-    def add_to_entry(self, string): self.entry.insert("end", string)
-
-    def clear(self): self.entry.delete(0, "end")
+    def add(self, string):
+        self.entry.insert("end", string)
 
     def backspace(self, event=None):
-        content = self.entry.get()
-        self.entry.delete(len(content) - 1)
+        pos = self.entry.index("insert")
+        self.entry.delete(pos - 1)
+        return "break"
 
-    def calculate(self, event=None):
-        content = self.entry.get().replace("^", "**").replace("x", "*").lstrip("0")
+    def calc(self, neget=False):
+        string = self.entry.get().translate(self.trantable).lstrip("0")
         try:
-            result = eval(content)
+            res = eval(string)
             self.entry.delete(0, "end")
-            self.entry.insert(0, result)
+            self.entry.insert(0, -res if neget else res)
         except:
-            msg.showerror("Error", "Value error")
+            msg.showerror(title="Error", message="Value Error")
+
+    def validate(self, string):
+        return all(c in self.valid_inputs for c in string)
 
 
-app = Calculator()
-app.mainloop()
+Calculator().mainloop()
